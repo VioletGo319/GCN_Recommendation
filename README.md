c5f49bde0e32c207a0a64645515cb92240e50921
Nischal Subedi <nischalsubedi12369@gmail.com>
2025-09-24 22:09:10 -0400

    initiation of the project

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # GCN_Recommendation
+
+# Idea 
+
+Recommendation System
+
+1) Get data
+2) Build a minimal working prototype from the data (build from scratch first (toy example)) and then scale with libraries 
+3) Try out various new methods - (1. GEE 2. how LightGCN is better? where does GCN perform better than LGCN? what methods to come to make these even better? )
+4) Abalation study
+5) Publishable paper
+
+
+
+To Do:
+
+we have:
+ # user id 
+ # item id 
+ # combination id 
+
+ now implement the LightGCN paper.
+
+ https://medium.com/@xiaolancara/explanation-and-practice-of-lightweight-graph-convolutional-network-lightgcn-ac30ac38469c
+
+ http://dmlab.kaist.ac.kr/~kijungs/papers/lightgcnppRecSys2024.pdf
+ https://arxiv.org/pdf/2503.14213
+
+
+ ![alt text](image.png)
+
---
+++ gcn_recommendation.py
@@ -0,0 +1,26 @@
+import pandas as pd 
+
+
+
+
+
+if __name__ == "__main__":
+
+
+    user_list = pd.read_csv('https://raw.githubusercontent.com/kuandeng/LightGCN/refs/heads/master/Data/amazon-book/user_list.txt', dtype={"org_id": str, "remap_id": int},
+    sep = '\s+')
+    user_list.rename(columns = {"org_id":"user_id"}, inplace = True)
+
+    
+    train_list = pd.read_csv('https://raw.githubusercontent.com/kuandeng/LightGCN/refs/heads/master/Data/amazon-book/train.txt')
+    test_list = pd.read_csv('https://raw.githubusercontent.com/kuandeng/LightGCN/refs/heads/master/Data/amazon-book/test.txt')
+
+    
+    item_list = pd.read_csv('https://raw.githubusercontent.com/kuandeng/LightGCN/master/Data/amazon-book/item_list.txt', sep=r"\s+", engine="python",
+                        dtype={"org_id": str, "remap_id": int})
+    item_list.rename(columns = {"org_id":"item_id"}, inplace = True)
+
+
+
+
+
